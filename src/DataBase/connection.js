769754681c4215974dbc859d9cc6192d30180766
Thiago Santos <thiagohsantosths@gmail.com>
2022-11-07 23:17:30 -0300

    Pequenos ajuste em connection.js

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -1,16 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./database.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
 
-    console.log("Connected to OliveiraTrade's database!");
-});
-
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-
-    console.log("Closed connection to OliveiraTrade's database!");
-});
+exports.open = (database) => {
+    let oi = new sqlite3.Database(database, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    
+        console.log("Connected to OliveiraTrade's database!");
+    });
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./src/Routes/index')(app);
+
+require('./Routes/index')(app);
 
 app.use(cors());
 app.use(express.json());
